c9f3bcfdade7ab2524d9762b4b4e7ee253c58018
coleleg <coleleg@gmail.com>
2022-04-10 14:11:28 -0400

    begin add header

---
+++ disney-plus-clone/src/components/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src="/images/logo.svg" />
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+            </NavMenu>
+        </Nav>
+    )
+};
+
+const Nav = styled.div`
+    height: 70px;
+    background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`;
+
+const Logo = styled.img`
+    width: 80px;
+`;
+
+const NavMenu = styled.div`
+    display: flex;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+        }
+
+    }
+
+`;
+
+export default Header;
