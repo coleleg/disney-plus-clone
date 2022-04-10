8df2e03b08e114d6db02d919f31ec43b42626263
coleleg <coleleg@gmail.com>
2022-04-10 18:58:42 -0400

    add all control buttons

--- disney-plus-clone/src/components/Detail.js
+++ disney-plus-clone/src/components/Detail.js
@@ -11,7 +11,20 @@ function Detail() {
                 <img src="/images/viewers-disney.png" />
             </ImgTitle>
             <Controls>
-
+                <PlayBtn>
+                    <img src = "/images/play-icon-black.png" />
+                    <span>PLAY</span>
+                </PlayBtn>
+                <TrailerBtn>
+                <img src = "/images/play-icon-white.png" />
+                <span>TRAILER</span>
+                </TrailerBtn>
+                <AddBtn>
+                    <span>+</span>
+                </AddBtn>
+                <GroupBtn>
+                    <img src="/images/group-icon.png" />
+                </GroupBtn>
             </Controls>
         </Container>
 
@@ -59,7 +72,59 @@ const ImgTitle = styled.div`
 `;
 
 const Controls = styled.div`
-    
+    display: flex;
+    align-items: center;
+`;
+
+const PlayBtn = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    padding: 0px 24px;
+    margin-right: 22px;
+    margin-left: 22px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    backgound: rgb (249, 249, 249);
+    border: none;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+`;
+
+const TrailerBtn = styled(PlayBtn)`
+    background: rgb(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+`;
+
+const AddBtn = styled.button`
+    height: 44px;
+    width: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    background: rgb(0, 0, 0, 0.6);
+    border: 1px solid rgb(249, 249, 249);
+    cursor: pointer;
+    margin-right: 16px;
+
+    span {
+        font-size: 30px;
+        color: rgb(249, 249, 249);
+    }
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+`;
+
+const GroupBtn = styled(AddBtn)`
+    background: rgb(0,0,0);
 `;
 
 export default Detail;
